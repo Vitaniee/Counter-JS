7ae39b2900c51b676072f5a082f8a9a8e2c9401b
Vitaniee <vitaniecao@gmail.com>
2023-01-06 11:02:54 +0800

    update increment button

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
  * @Description: 
  * @Author: Xinyue Cao
  * @Date: 2023-01-06 10:40:11
- * @LastEditTime: 2023-01-06 10:41:34
+ * @LastEditTime: 2023-01-06 11:02:50
  * @LastEditors: Xinyue Cao
  */
-// console.log(5)
 
+
+let count = 0
+let countEl = document.getElementById("count-el")
 function increment() {
     console.log("INCREMENT clicked!")
+    count = count+1
+    countEl.innerText = count
 }
