b6c7c1868fff0b8e1547bc926d7b32bc1a4603fc
Vitaniee <vitaniecao@gmail.com>
2023-01-06 10:42:33 +0800

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: Xinyue Cao
+ * @Date: 2023-01-06 10:40:11
+ * @LastEditTime: 2023-01-06 10:41:34
+ * @LastEditors: Xinyue Cao
+ */
+// console.log(5)
+
+function increment() {
+    console.log("INCREMENT clicked!")
+}
