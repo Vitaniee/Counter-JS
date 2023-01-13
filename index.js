5933128457b9508493c1dd35878ddbe48addf26c
Vitaniee <vitaniecao@gmail.com>
2023-01-13 14:08:36 +0800

    update

--- index.js
+++ index.js
@@ -2,15 +2,22 @@
  * @Description: 
  * @Author: Xinyue Cao
  * @Date: 2023-01-06 10:40:11
- * @LastEditTime: 2023-01-06 11:02:50
+ * @LastEditTime: 2023-01-13 13:49:44
  * @LastEditors: Xinyue Cao
  */
 
-
-let count = 0
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let count = 0
+
 function increment() {
-    console.log("INCREMENT clicked!")
-    count = count+1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
